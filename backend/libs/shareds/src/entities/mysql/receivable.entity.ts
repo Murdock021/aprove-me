cb431285aac5df014a6c3e2f14e546f4026d733c
diogomachadocmb@gmail.com <diogomachadocmb@gmail.com>
2024-05-21 23:30:20 -0300

    Fix: Validation repair

--- backend/libs/shareds/src/entities/mysql/receivable.entity.ts
+++ backend/libs/shareds/src/entities/mysql/receivable.entity.ts
@@ -1,16 +1,23 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsUUID, IsNumber, IsDate, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
 
 @Entity()
 export class Receivable {
   @PrimaryGeneratedColumn('uuid')
+  @IsUUID()
   id: string;
 
   @Column('float')
+  @IsNumber()
   value: number;
 
   @Column('date')
+  @IsDate()
+  @Type(() => Date)
   emissionDate: Date;
 
   @Column('int')
+  @IsInt()
   assignor: number;
 }
