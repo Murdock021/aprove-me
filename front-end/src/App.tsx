ab56409577f1692ae2aaafacb7529da6d9e43a23
diogomachadocmb@gmail.com <diogomachadocmb@gmail.com>
2024-07-17 23:20:39 -0300

    Feat: Creation of server implementation

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,9 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./view/pages/login";
+import { ClientRoutes } from "./servers/routes";
 
 function App() {
   return (
-<h1>Teste</h1>
-  );
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route
+            path="/*"
+            element={
+              <ClientRoutes />
+            }
+          />
+        </Routes>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ front-end/src/servers/routes/client.routes.tsx
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import { Route, Routes } from "react-router-dom";
+import { Home } from "@/view/pages/home";
+import { HeaderNav } from "@/view/layouts/general/header.layout";
+import PrivateRoute from "../context/PrivateRoute";
+import SettingsLayout from "@/view/layouts/signup/signup.layout";
+import SettingsAccountPage from "@/view/pages/signup";
+
+const ClientRoutes = () => {
+  console.log("ClientRoutes");
+  return (
+    <Routes>
+      <Route
+        path="home"
+        element={
+          <Routes>
+            <Route index element={
+              <HeaderNav>
+                <Home />
+              </HeaderNav>
+            } />
+          </Routes>
+        }
+      />
+      <Route
+        path="signup"
+        element={
+          <Routes>
+            <Route index element={
+              <SettingsLayout>
+                <SettingsAccountPage />
+              </SettingsLayout>
+            } />
+          </Routes>
+        }
+      />
+    </Routes>
+  );
+};
+export default ClientRoutes;
