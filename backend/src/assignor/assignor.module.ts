01e9f90dbc4c39c12aa20b337082e89729e6c959
diogomachadocmb@gmail.com <diogomachadocmb@gmail.com>
2024-07-18 22:20:12 -0300

    Feat: Creation of the users DTO and user service

--- backend/src/assignor/assignor.module.ts
+++ backend/src/assignor/assignor.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AssignorService } from './assignor.service';
-
 import { AssignorRepository } from './assignor.repository';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule],
   controllers: [AbortController],
   providers: [AssignorService, AssignorRepository, PrismaService],
 })
