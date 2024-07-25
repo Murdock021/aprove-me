db8d551ba0034efe0947a9822c74919a54116a91
diogomachadocmb@gmail.com <diogomachadocmb@gmail.com>
2024-07-25 17:30:00 -0300

    Fix: Folder rename

--- front-end/src/servers/config.ts
+++
@@ -1,6 +0,0 @@
-export const CONFIG = {
-  host: "https://api.arp.evolnex.com/",
-  port_auth: "auth",
-  port_general: "general",
-  port_aws: "aws-domains",
-};
--- front-end/src/servers/routes/index.tsx
+++
@@ -1,1 +0,0 @@
-export { default as ClientRoutes } from './client.routes';
